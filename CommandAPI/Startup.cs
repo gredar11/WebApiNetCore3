dc0cab896c5eea019df8f385a0061e1365c2a4ed
Эльдар Букеев <bukeevel@gmail.com>
2022-09-06 11:04:13 +0300

    Added DbContext to our services and created connection string.

--- CommandAPI/Startup.cs
+++ CommandAPI/Startup.cs
@@ -1,3 +1,4 @@
+using CommandAPI.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -7,15 +8,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace CommandAPI
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CommandContext>(options =>
+            {
+                options.UseNpgsql(Configuration["PostgreSqlConnection"]);
+            });
             services.AddControllers();
         }
 
