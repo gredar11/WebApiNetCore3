bdcaac0a763967d814d73076c13b5c436991eeea
Эльдар Букеев <bukeevel@gmail.com>
2022-09-07 15:48:35 +0300

    Added tests for controller

---
+++ test/CommandAPI.Tests/CommandTests.cs
@@ -0,0 +1,46 @@
+﻿using CommandAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CommandAPI.Tests
+{
+    public class CommandTests:IDisposable
+    {
+        Command testCommand;
+        public CommandTests()
+        {
+            testCommand = new Command
+            {
+                HowTo = "Do something",
+                Platform = "XUnit",
+                CommandLine = "dotnet test"
+            };
+        }
+        [Fact]
+        public void CanChangeHowTo()
+        {
+            testCommand.HowTo = "Execute unit test";
+            Assert.Equal("Execute unit test", testCommand.HowTo);
+        }
+        [Fact]
+        public void CanChangePlatform()
+        {
+            testCommand.Platform = "Execute unit test";
+            Assert.Equal("Execute unit test", testCommand.Platform);
+        }
+        [Fact]
+        public void CanChangeCommandLine()
+        {
+            testCommand.CommandLine = "Execute unit test";
+            Assert.Equal("Execute unit test", testCommand.CommandLine);
+        }
+        public void Dispose()
+        {
+            testCommand = null;
+        }
+    }
+}
---
+++ test/CommandAPI.Tests/CommandsControllerTests.cs
@@ -0,0 +1,82 @@
+﻿using AutoMapper;
+using CommandAPI.Controllers;
+using CommandAPI.Data;
+using CommandAPI.Dtos;
+using CommandAPI.Models;
+using CommandAPI.Profiles;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+namespace CommandAPI.Tests
+{
+    public class CommandsControllerTests : IDisposable
+    {
+        Mock<ICommandAPIRepo> mockRepo;
+        CommandsProfile realProfile;
+        MapperConfiguration configuration;
+        IMapper mapper;
+        public CommandsControllerTests()
+        {
+            mockRepo = new Mock<ICommandAPIRepo>();
+            realProfile = new CommandsProfile();
+            configuration = new MapperConfiguration(cfg => cfg.
+            AddProfile(realProfile));
+            mapper = new Mapper(configuration);
+        }
+        public void Dispose()
+        {
+            mockRepo = null;
+            mapper = null;
+            configuration = null;
+            realProfile = null;
+        }
+        [Fact]
+        public void GetCommandItems_ReturnsZeroItems_WhenDbIsEmpty()
+        {
+            // Arrange
+            mockRepo.Setup(repo => repo.GetAllCommands()).Returns(GetCommands(0));
+           
+            var controller = new CommandsController(mockRepo.Object, mapper);
+
+            // act
+            var result = controller.GetAllCommands();
+            // assert
+            Assert.IsType<OkObjectResult>(result.Result);
+        }
+        private List<Command> GetCommands(int num)
+        {
+            var commands = new List<Command>();
+            if (num > 0)
+            {
+                commands.Add(new Command
+                {
+                    Id = 0,
+                    HowTo = "How to generate a migration",
+                    CommandLine = "dotnet ef migrations add <Name of Migration>",
+                    Platform = ".Net Core EF"
+                });
+            }
+            return commands;
+        }
+        [Fact]
+        public void GetAllCommands_ReturnsOneItem_WhenDBHasOneResource()
+        {
+            //Arrange
+            mockRepo.Setup(repo =>
+            repo.GetAllCommands()).Returns(GetCommands(1));
+            var controller = new CommandsController(mockRepo.Object, mapper);
+            //Act
+            var result = controller.GetAllCommands();
+            //Assert
+            var okResult = result.Result as OkObjectResult;
+            var commands = okResult.Value as List<CommandReadDto>;
+            Assert.Single(commands);
+        }
+    }
+
+}
